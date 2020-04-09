81df0bdf1f836850b1d556e9b7e2ed6f4984f4cf
Joseph H Kennedy <me@jhkennedy.org>
2020-04-08 16:18:36 -0800

    Roll back hyp3 entrypoint updates to get logs

--- hyp3_autorift/__main__.py
+++ hyp3_autorift/__main__.py
@@ -5,18 +5,29 @@ import os
 import shutil
 from datetime import datetime
 
-import hyp3proclib
+from hyp3proclib import (
+    build_output_name_pair,
+    earlier_granule_first,
+    failure,
+    process,
+    record_metrics,
+    success,
+    upload_product,
+    zip_dir,
+)
 from hyp3proclib.db import get_db_connection
+from hyp3proclib.file_system import cleanup_workdir
 from hyp3proclib.logger import log
 from hyp3proclib.proc_base import Processor
 
 import hyp3_autorift
-from hyp3_autorift.process import process as autorift_process
 
 
 def hyp3_process(cfg, n):
     try:
-        g1, g2 = hyp3proclib.earlier_granule_first(cfg['granule'], cfg['other_granules'][0])
+        log.info(f'Processing autoRIFT-ISCE pair "{cfg["sub_name"]}" for "{cfg["username"]}"')
+
+        g1, g2 = earlier_granule_first(cfg['granule'], cfg['other_granules'][0])
 
         d1 = datetime.strptime(g1[17:25], '%Y%m%d')
         d2 = datetime.strptime(g2[17:25], '%Y%m%d')
@@ -25,28 +36,33 @@ def hyp3_process(cfg, n):
                             f'from {d1.strftime("%Y-%m-%d")} to {d2.strftime("%Y-%m-%d")}.'
 
         cfg['ftd'] = '_'.join([g1[17:17+15], g2[17:17+15]])
+        log.debug(f'FTD dir is: {cfg["ftd"]}')
 
-        if not cfg['skip_processing']:
-            log.info(f'Process starting at {datetime.now()}')
-            launch_dir = os.getcwd()
-            os.chdir(cfg['workdir'])
-
-            autorift_process(g1, g2, download=True, process_dir=cfg['ftd'], product=True)
-
-            os.chdir(launch_dir)
-        else:
-            log.info('Processing skipped!')
-            log.info(f'Command would be: autorift_proc_pair {g1} {g2} --process-dir {cfg["ftd"]} --download '
-                     f'--product-dir')
-            cfg['log'] += "(debug mode)"
-
-        cfg['success'] = True
-        hyp3proclib.update_completed_time(cfg)
-
-        # FIXME: But, why?
-        cfg["granule_name"] = cfg["granule"]
-        cfg["processes"] = [cfg["proc_id"], ]
-        cfg["subscriptions"] = [cfg["sub_id"], ]
+        process(
+            cfg, 'autorift_proc_pair',
+            [f'{g1}.zip', f'{g2}.zip', '--process-dir', f'{cfg["ftd"]}', '--download', '--product-dir']
+        )
+        # if not cfg['skip_processing']:
+        #     log.info(f'Process starting at {datetime.now()}')
+        #     launch_dir = os.getcwd()
+        #     os.chdir(cfg['workdir'])
+        #
+        #     autorift_process(f'{g1}.zip', f'{g2}.zip', download=True, process_dir=cfg['ftd'], product=True)
+        #
+        #     os.chdir(launch_dir)
+        # else:
+        #     log.info('Processing skipped!')
+        #     log.info(f'Command would be: autorift_proc_pair {g1}.zip {g2}.zip --process-dir {cfg["ftd"]} --download '
+        #              f'--product-dir')
+        #     cfg['log'] += "(debug mode)"
+        #
+        # cfg['success'] = True
+        # update_completed_time(cfg)
+        #
+        # # FIXME: But, why?
+        # cfg["granule_name"] = cfg["granule"]
+        # cfg["processes"] = [cfg["proc_id"], ]
+        # cfg["subscriptions"] = [cfg["sub_id"], ]
 
         tmp_product_dir = os.path.join(cfg['workdir'], 'PRODUCT')
         if not os.path.isdir(tmp_product_dir):
@@ -54,7 +70,7 @@ def hyp3_process(cfg, n):
             log.error('Processing failed')
             raise Exception('Processing failed: PRODUCT directory not found')
         else:
-            out_name = hyp3proclib.build_output_name_pair(g1, g2, cfg['workdir'], cfg['suffix'])
+            out_name = build_output_name_pair(g1, g2, cfg['workdir'], cfg['suffix'])
             log.info(f'Output name: {out_name}')
 
             product_dir = os.path.join(cfg['workdir'], out_name)
@@ -73,22 +89,22 @@ def hyp3_process(cfg, n):
             #  * citation
             #  * phase_png (?)
 
-            hyp3proclib.zip_dir(product_dir, zip_file)
+            zip_dir(product_dir, zip_file)
 
             cfg['final_product_size'] = [os.stat(zip_file).st_size, ]
             cfg['original_product_size'] = 0
 
             with get_db_connection('hyp3-db') as conn:
-                hyp3proclib.record_metrics(cfg, conn)
-                hyp3proclib.upload_product(zip_file, cfg, conn)
-                hyp3proclib.success(conn, cfg)
+                record_metrics(cfg, conn)
+                upload_product(zip_file, cfg, conn)
+                success(conn, cfg)
 
     except Exception as e:
         log.exception('autoRIFT processing failed!')
         log.exception('Notifying user')
-        hyp3proclib.failure(cfg, str(e))
+        failure(cfg, str(e))
 
-    hyp3proclib.file_system.cleanup_workdir(cfg)
+    cleanup_workdir(cfg)
 
     log.info('autoRIFT done')
 
@@ -98,7 +114,7 @@ def main():
     Main entrypoint for hyp3_autorift
     """
     processor = Processor(
-        'autorift', hyp3_process, sci_version=hyp3_autorift.__version__
+        'autorift_isce', hyp3_process, sci_version=hyp3_autorift.__version__
     )
     processor.run()
 
