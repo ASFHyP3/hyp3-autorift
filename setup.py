4425937328c802262a621e06561ae705e7174f05
Joseph H Kennedy <me@jhkennedy.org>
2020-06-25 14:20:00 -0800

    fix py version req.

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
         ],
 
-    python_requires='~=3.7',
+    python_requires='~=3.6',
 
     install_requires=[
         # FIXME: use boto3 instead of requests for download in hyp3_autorift.io.fetch_jpl_tifs
