275db339420bda11cb4eb813488207012fcf354f
Joseph H Kennedy <me@jhkennedy.org>
2020-12-09 12:15:09 -0900

    check outfile in make_browse test

--- tests/test_image.py
+++ tests/test_image.py
@@ -5,10 +5,12 @@ from hyp3_autorift import image
 
 
 def test_make_browse(tmp_path):
-    out_file = tmp_path / 'test.png'
+    image_file = tmp_path / 'test.png'
     data = np.random.rand(10, 10) * 700
 
-    image.make_browse(out_file, data)
+    out_file = image.make_browse(image_file, data)
+    assert out_file == image_file
     assert out_file.is_file()
+
     with Image.open(out_file) as img:
         assert img.format == 'PNG'
