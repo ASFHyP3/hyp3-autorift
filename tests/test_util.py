2e5de2c41d386f87f33af709cfa99366656a03f5
Joseph H Kennedy <me@jhkennedy.org>
2021-11-16 16:25:08 -0900

    fix lint

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,6 @@
 from hyp3_autorift import util
 
+
 def test_get_cononical_paramter_file():
     CANONICAL_URL = '/vsicurl/http://its-live-data.s3.amazonaws.com/autorift_parameters/v001/autorift_landice_0120m.shp'
     assert util.get_cononical_paramter_file(CANONICAL_URL) == CANONICAL_URL
