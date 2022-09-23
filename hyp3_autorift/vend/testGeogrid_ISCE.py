f2b560b37f756370a5300fe1c57e57b84feb63d1
Joseph H Kennedy <me@jhkennedy.org>
2022-09-22 22:02:19 -0800

    sync testgeogrid scripts

--- hyp3_autorift/vend/testGeogrid_ISCE.py
+++ hyp3_autorift/vend/testGeogrid_ISCE.py
@@ -176,6 +176,9 @@ def coregisterLoadMetadataOptical(indir_m, indir_s, **kwargs):
     elif re.findall("L[EO]07_",DS.GetDescription()).__len__() > 0:
         nameString = os.path.basename(DS.GetDescription())
         info.time = nameString.split('_')[3]
+    elif re.findall("LT0[45]_",DS.GetDescription()).__len__() > 0:
+        nameString = os.path.basename(DS.GetDescription())
+        info.time = nameString.split('_')[3]
     elif 'sentinel-s2-l1c' in indir_m or 's2-l1c-us-west-2' in indir_m:
         s2_name = kwargs['reference_metadata']['id']
         info.time = s2_name.split('_')[2]
@@ -197,6 +200,9 @@ def coregisterLoadMetadataOptical(indir_m, indir_s, **kwargs):
         nameString1 = os.path.basename(DS1.GetDescription())
         info1.time = nameString1.split('_')[3]
     elif re.findall("L[EO]07_",DS1.GetDescription()).__len__() > 0:
+        nameString1 = os.path.basename(DS1.GetDescription())
+        info1.time = nameString1.split('_')[3]
+    elif re.findall("LT0[45]_",DS1.GetDescription()).__len__() > 0:
         nameString1 = os.path.basename(DS1.GetDescription())
         info1.time = nameString1.split('_')[3]
     elif 'sentinel-s2-l1c' in indir_s or 's2-l1c-us-west-2' in indir_s:
