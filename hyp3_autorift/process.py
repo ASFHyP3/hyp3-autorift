c3115ab12f5cd579308277f4256e045082e16da5
Joseph H Kennedy <me@jhkennedy.org>
2022-10-18 16:49:04 -0400

    strip excessive print statement

--- hyp3_autorift/process.py
+++ hyp3_autorift/process.py
@@ -351,11 +351,6 @@ def process(reference: str, secondary: str, parameter_file: str = DEFAULT_PARAME
         gdal.AllRegister()
 
         from hyp3_autorift.vend.testautoRIFT_ISCE import generateAutoriftProduct
-        print(f'generateAutoriftProduct(\n'
-              f'    {reference_path}, {secondary_path}, nc_sensor={platform}, optical_flag=False, ncname=None,'
-              f'    geogrid_run_info={geogrid_info}, {parameter_info["autorift"]},'
-              f'    parameter_file={DEFAULT_PARAMETER_FILE.replace("/vsicurl/", "")},'
-              f')')
         netcdf_file = generateAutoriftProduct(
             reference_path, secondary_path, nc_sensor=platform, optical_flag=False, ncname=None,
             geogrid_run_info=geogrid_info, **parameter_info['autorift'],
