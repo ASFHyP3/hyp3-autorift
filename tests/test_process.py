f2a240881d12e3614b4daab91d4b1d49e7084d1b
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2022-11-03 14:16:54 -0800

    remove extra slash

--- tests/test_process.py
+++ tests/test_process.py
@@ -129,7 +129,7 @@ def test_get_lc2_path():
 
 @responses.activate
 def test_get_s2_metadata_not_found():
-    responses.add(responses.GET, f'{process.S2_GRANULE_DIR}////foo.SAFE/manifest.safe', status=404)
+    responses.add(responses.GET, f'{process.S2_GRANULE_DIR}/////foo.SAFE/manifest.safe', status=404)
     with pytest.raises(ValueError):
         process.get_s2_metadata('foo')
 
@@ -145,7 +145,7 @@ def test_get_s2_path(test_data_directory):
     with open(f'{test_data_directory}/{scene_name}.manifest.safe', 'r') as f:
         manifest_text = f.read()
     path = process.get_s2_path(manifest_text, scene_name)
-    assert path == '/vsicurl/https://storage.googleapis.com/gcp-public-data-sentinel-2/tiles//./GRANULE' \
+    assert path == '/vsicurl/https://storage.googleapis.com/gcp-public-data-sentinel-2/tiles/./GRANULE' \
                    '/S2A_OPER_MSI_L1C_TL_SGS__20160616T181414_A005139_T29QKF_N02.04/IMG_DATA' \
                    '/S2A_OPER_MSI_L1C_TL_SGS__20160616T181414_A005139_T29QKF_B08.jp2'
 
@@ -153,7 +153,7 @@ def test_get_s2_path(test_data_directory):
     with open(f'{test_data_directory}/{scene_name}.manifest.safe', 'r') as f:
         manifest_text = f.read()
     path = process.get_s2_path(manifest_text, scene_name)
-    assert path == '/vsicurl/https://storage.googleapis.com/gcp-public-data-sentinel-2/tiles//./GRANULE' \
+    assert path == '/vsicurl/https://storage.googleapis.com/gcp-public-data-sentinel-2/tiles/./GRANULE' \
                    '/L1C_T38EMQ_A016290_20200419T060719/IMG_DATA/T38EMQ_20200419T060719_B08.jp2'
 
 
