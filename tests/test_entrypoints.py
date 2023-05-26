392c30a15a8cdf898f55f37614663ca90d0e587f
Joseph H Kennedy <me@jhkennedy.org>
2023-05-26 11:55:57 -0800

    fix tests

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -4,5 +4,5 @@ def test_hyp3_autorift(script_runner):
 
 
 def test_autorift_proc_pair(script_runner):
-    ret = script_runner.run('autorift_proc_pair', '-h')
+    ret = script_runner.run('s1_correction', '-h')
     assert ret.success
