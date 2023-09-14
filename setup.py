98472493200beab941dfd3d5922038399b4ca174
Joseph H Kennedy <me@jhkennedy.org>
2023-09-14 15:52:20 -0800

    revert #229

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         'boto3',
         'botocore',
         'gdal',
-        'h5netcdf',
         'hyp3lib==1.7.0',
         'matplotlib',
         'netCDF4',
@@ -45,7 +44,6 @@ setup(
         'pyproj',
         'requests',
         'scipy',
-        'xarray',
     ],
 
     extras_require={
