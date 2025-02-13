72201b34a11e109d592b2853128476dad5744a26
Andrew Player <andrewplayer3@gmail.com>
2025-02-13 16:27:16 -0500

    refactoring

--- src/hyp3_autorift/s1_isce3.py
+++ src/hyp3_autorift/s1_isce3.py
@@ -54,33 +54,6 @@ def process_sentinel1_burst_isce3(burst_granule_ref, burst_granule_sec, is_opera
     )
 
 
-def process_sentinel1_slc_isce3(slc_ref, slc_sec):
-    esa_username, esa_password = get_esa_credentials()
-    esa_credentials = (esa_username, esa_password)
-
-    for scene in [slc_ref, slc_sec]:
-        scene_url = get_download_url(scene)
-        download_file(scene_url, chunk_size=5242880)
-
-    safe_ref = sorted(glob.glob('./*.zip'))[0]
-    safe_sec = sorted(glob.glob('./*.zip'))[1]
-    orbit_ref, _ = downloadSentinelOrbitFile(slc_ref, esa_credentials=esa_credentials)
-    orbit_sec, _ = downloadSentinelOrbitFile(slc_sec, esa_credentials=esa_credentials)
-    burst_ids_ref = get_burst_ids(safe_ref, orbit_ref)
-    burst_ids_sec = get_burst_ids(safe_sec, orbit_sec)
-
-    get_dem_for_safes(safe_ref, safe_sec)
-
-    return process_slc(
-        safe_ref,
-        safe_sec,
-        orbit_ref,
-        orbit_sec,
-        burst_ids_ref,
-        burst_ids_sec
-    )
-
-
 def process_burst(
     safe_ref,
     safe_sec, 
@@ -126,6 +99,33 @@ def process_burst(
     return netcdf_file
 
 
+def process_sentinel1_slc_isce3(slc_ref, slc_sec):
+    esa_username, esa_password = get_esa_credentials()
+    esa_credentials = (esa_username, esa_password)
+
+    for scene in [slc_ref, slc_sec]:
+        scene_url = get_download_url(scene)
+        download_file(scene_url, chunk_size=5242880)
+
+    safe_ref = sorted(glob.glob('./*.zip'))[0]
+    safe_sec = sorted(glob.glob('./*.zip'))[1]
+    orbit_ref, _ = downloadSentinelOrbitFile(slc_ref, esa_credentials=esa_credentials)
+    orbit_sec, _ = downloadSentinelOrbitFile(slc_sec, esa_credentials=esa_credentials)
+    burst_ids_ref = get_burst_ids(safe_ref, orbit_ref)
+    burst_ids_sec = get_burst_ids(safe_sec, orbit_sec)
+
+    get_dem_for_safes(safe_ref, safe_sec)
+
+    return process_slc(
+        safe_ref,
+        safe_sec,
+        orbit_ref,
+        orbit_sec,
+        burst_ids_ref,
+        burst_ids_sec
+    )
+
+
 def process_slc(
     safe_ref,
     safe_sec,
@@ -136,8 +136,8 @@ def process_slc(
 ):
     write_yaml(safe_ref, orbit_ref)
     s1_cslc.run('s1_cslc.yaml', 'radar')
-    print('Bursts ref', burst_ids_ref)
-    print('Bursts sec', burst_ids_sec)
+    print(f'Reference Burst IDs: {burst_ids_ref}')
+    print(f'Secondary burst IDs: {burst_ids_sec}')
     burst_ids = list(set(burst_ids_sec) & set(burst_ids_ref))
 
     for burst_id_sec in burst_ids:
@@ -145,8 +145,8 @@ def process_slc(
         write_yaml(safe_sec, orbit_sec, burst_id_sec)
         s1_cslc.run('s1_cslc.yaml', 'radar')
 
-    merge_swaths(ref=False)
-    merge_swaths()
+    merge_swaths(safe_ref, orbit_ref)
+    merge_swaths(safe_sec, orbit_sec, is_ref=False)
 
     meta_r = loadMetadataSlc(safe_ref, orbit_ref)
     meta_temp = loadMetadataSlc(safe_sec, orbit_sec)
@@ -176,12 +176,40 @@ def process_slc(
     return netcdf_file
 
 
+def read_slc_gdal(slc_path):
+    ds = gdal.Open(slc_path)
+    tran = ds.GetGeoTransform()
+    proj = ds.GetProjection()
+    band = ds.GetRasterBand(1)
+    slc_arr = band.ReadAsArray()
+    del band, ds
+    return slc_arr, tran, proj
+
+
+def write_slc_gdal(
+    data,
+    out_path,
+    transform,
+    projection,
+    num_rng_samples,
+    num_az_samples
+):
+    nodata = 0
+    driver = gdal.GetDriverByName('ENVI')
+    out_raster = driver.Create(out_path, num_rng_samples, num_az_samples, 1, gdal.GDT_CFloat32)
+    out_raster.SetGeoTransform(transform)
+    out_raster.SetProjection(projection)
+    out_band = out_raster.GetRasterBand(1)
+    out_band.SetNoDataValue(nodata)
+    out_band.WriteArray(data)
+    out_band.FlushCache()
+    del out_raster
+
+
 def merge_swaths(safe, orbit, is_ref=True, swaths=[1, 2, 3]):
-    safe_date = datetime.strptime(safe.split('_')[5], '%Y%m%dT%H%M%S')
-    orbit_date =  datetime.strptime(orbit.split('_')[6], 'V%Y%m%dT%H%M%S')
-    path = os.path.abspath(safe)
+    safe_path = os.path.abspath(safe)
     orbit_path = os.path.abspath(orbit)
-    burst_ids = get_burst_ids(path, orbit_path)
+    burst_ids = get_burst_ids(safe_path, orbit_path)
     product_path = './product/*' if is_ref else './product_sec/*'
     output_path = 'reference.slc' if is_ref else 'secondary.slc'
     burst_files = sorted(glob.glob(product_path))
@@ -258,17 +286,13 @@ def merge_swaths(safe, orbit, is_ref=True, swaths=[1, 2, 3]):
         print('Offsets', rng_offset, az_offset)
 
         slc = 'swath_iw'+str(swath)+'.slc'
-        ds = gdal.Open(slc)
-        band = ds.GetRasterBand(1)
-        if swath == 1:
-            tran = ds.GetGeoTransform()
-            proj = ds.GetProjection()
-        slc_array = band.ReadAsArray()
-        del band, ds
+        slc_array, tran_temp, proj_temp = read_slc_gdal(slc)
 
         az_end_index = az_offset+slc_array.shape[0]
         rng_end_index = rng_offset+slc_array.shape[1]
         if swath == 1:
+            tran = tran_temp
+            proj = proj_temp
             merged_array[az_offset:az_end_index, rng_offset:rng_end_index] = slc_array
         else:
             temp = merged_array[az_offset:az_end_index, rng_offset:rng_end_index]
@@ -276,163 +300,124 @@ def merge_swaths(safe, orbit, is_ref=True, swaths=[1, 2, 3]):
             merged_array[az_offset:az_end_index, rng_offset:rng_end_index][cond] = slc_array[cond]
             temp = None
 
-    nodata = 0
-    driver = gdal.GetDriverByName('ENVI')
-    out_raster = driver.Create(output_path, total_rng_samples, total_az_samples, 1, gdal.GDT_CFloat32)
-    out_raster.SetGeoTransform(tran)
-    out_raster.SetProjection(proj)
-    out_band = out_raster.GetRasterBand(1)
-    out_band.SetNoDataValue(nodata)
-    out_band.WriteArray(merged_array)
-    out_band.FlushCache()
-    del out_raster
+    write_slc_gdal(
+        merged_array,
+        output_path,
+        tran,
+        proj,
+        total_rng_samples,
+        total_az_samples
+    )
+
     subprocess.call('rm -rf swath_*iw*', shell=True)
 
 
-def merge_bursts_in_swath(swath, ref=True, outfile='output.slc', method='top'):
+def get_azimuth_reference_offsets(bursts):
+    sensing_start = bursts[0].sensing_start
+    az_time_interval = bursts[0].azimuth_time_interval
+    az_reference_offsets = []
+
+    for index in range(len(bursts)):
+        burst = bursts[index]
+        az_offset = burst.sensing_start + timedelta(seconds=(burst.first_valid_line * az_time_interval))
+
+        burst_start_index = int(np.round((az_offset - sensing_start).total_seconds() / az_time_interval))
+        burst_end_index = burst_start_index + (burst.last_valid_line - burst.first_valid_line) + 1
+
+        if index == 0:
+            start_index = burst_start_index
+
+        az_reference_offsets.append([burst_start_index, burst_end_index])
+        print(f'Burst {index}: {(burst_start_index, burst_end_index)}')
+
+    return az_reference_offsets, start_index
+
+
+def merge_bursts_in_swath(bursts, burst_files, swath, pol, is_ref=True, outfile='output.slc', method='top'):
     '''
     Merge burst products into single file.
     Simple numpy based stitching
     '''
-    safes = glob.glob('*.zip')
-    orbits = glob.glob('*.EOF')
-    dates_safes = [datetime.strptime(safe.split('_')[5], '%Y%m%dT%H%M%S') for safe in safes]
-    dates_orbits = [datetime.strptime(orbit.split('_')[6], 'V%Y%m%dT%H%M%S') for orbit in orbits]
-    argmin_safe = np.argsort(dates_safes)
-    argmin_orbit = np.argsort(dates_orbits)
+    num_bursts = len(bursts)
+    az_time_interval = bursts[0].azimuth_time_interval
+    num_az_samples, num_rng_samples = bursts[-1].shape
 
-    safe = safes[argmin_safe[0]]
-    orbit_file = orbits[argmin_orbit[0]]
+    last_burst_sensing_start = bursts[-1].sensing_start
+    burst_length = timedelta(seconds=(num_az_samples-1.0) * az_time_interval)
 
-    path = os.path.abspath(safe)
-    orbit_path = os.path.abspath(orbit_file)
+    sensing_start = bursts[0].sensing_start
+    sensing_end = last_burst_sensing_start + burst_length
 
-    pol = getPol(safe, orbit_path)
+    num_az_lines = 1 + int(np.round((sensing_end - sensing_start).total_seconds() / az_time_interval))
+    print(f'Expected Number of Azimuth Lines: {num_az_lines}')
 
-    burst_idst = get_burst_ids(path, orbit_path)
-    burst_ids = [burst_id for burst_id in burst_idst if 'iw'+str(swath) in burst_id]
-    if ref:
-        fileList = sorted(glob.glob('./product/*_iw'+str(swath)))
-    else:
-        fileList = sorted(glob.glob('./product_sec/*_iw'+str(swath)))
+    az_reference_offsets, merge_start_index = get_azimuth_reference_offsets(bursts)
 
-    # Check against metadata
-    if len(burst_ids) != len(fileList):
-        print('Warning : Not all the bursts were processed')
-
-    bursts = s1reader.load_bursts(safe, orbit_file, swath, pol)
-
-    dt = bursts[0].azimuth_time_interval
-    length, width = bursts[-1].shape
-
-    #######
-    tstart = bursts[0].sensing_start
-    tstartLast = bursts[-1].sensing_start
-    tend = (tstartLast + timedelta(seconds=(length-1.0) * dt))
-    nLines = int(np.round((tend - tstart).total_seconds() / dt)) + 1
-    print('Expected total nLines: ', nLines)
-
-    azReferenceOff = []
-    for index in range(len(burst_ids)):
+    merged_arr = np.zeros((num_az_lines, num_rng_samples), dtype=complex)
+    for index in range(num_bursts):
         burst = bursts[index]
-        soff = burst.sensing_start + timedelta(seconds=(burst.first_valid_line*dt))
-        start = int(np.round((soff - tstart).total_seconds() / dt))
-        end = start + (burst.last_valid_line - burst.first_valid_line) + 1
-
-        azReferenceOff.append([start, end])
-
-        print('Burst: ', index, [start, end])
-
-        if index == 0:
-            linecount = start
-
-    outArray = np.zeros((nLines, width), dtype=complex)
-
-    for index in range(len(burst_ids)):
-        curBurst = bursts[index]
-        curLimit = azReferenceOff[index]
-
-        folder = glob.glob(fileList[index]+'/*')[0]
-        slc = glob.glob(folder+'/*.slc')[0]
-        ds = gdal.Open(slc)
-        if index == 0:
-            tran = ds.GetGeoTransform()
-            proj = ds.GetProjection()
-        band = ds.GetRasterBand(1)
-        curMap = band.ReadAsArray()
-        band = None
-        ds = None
+        burst_limit = az_reference_offsets[index]
+        burst_slc = glob.glob(glob.glob(burst_files[index]+'/*')[0]+'/*.slc')[0]
+        burst_arr, tran, proj = read_slc_gdal(burst_slc)
 
         # If middle burst
         if index > 0:
-            topBurst = bursts[index-1]
-            folder = glob.glob(fileList[index-1]+'/*')[0]
-            slc = glob.glob(folder+'/*.slc')[0]
-            ds = gdal.Open(slc)
-            band = ds.GetRasterBand(1)
-            topMap = band.ReadAsArray()
-            band = None
-            ds = None
-            topLimit = azReferenceOff[index-1]
+            prev_burst = bursts[index-1]
+            prev_burst_slc = glob.glob(glob.glob(burst_files[index-1]+'/*')[0]+'/*.slc')[0]
+            prev_burst_arr, _, _ = read_slc_gdal(prev_burst_slc)
+            prev_burst_limit = az_reference_offsets[index-1]
 
-            olap = topLimit[1] - curLimit[0]
+            burst_overlap = prev_burst_limit[1] - burst_limit[0]
+            burst_start_index = burst_overlap
+            if burst_overlap <= 0:
+                raise ValueError(f'No overlap between bursts {index} and {index-1} in swath {swath}')
+            print(f'Burst Overlap: {burst_overlap}')
 
-            print("olap: ", olap)
+            burst_subset = burst_arr[burst.first_valid_line:burst.last_valid_line + 1, :][:burst_overlap, :]
+            prev_burst_subset = prev_burst_arr[prev_burst.first_valid_line:prev_burst.last_valid_line + 1, :][-burst_overlap:, :]
 
-            if olap <= 0:
-                raise Exception('No Burst Overlap')
+            match method:
+                case 'avg':
+                    data = 0.5 * (burst_subset + prev_burst_subset)
+                case 'top':
+                    data = prev_burst_subset
+                case 'bot':
+                    data = burst_subset
+                case _:
+                    raise ValueError(f'Method should one of "top", "bot", or "avg", but got {method}.')
 
-            topData = topMap[topBurst.first_valid_line:topBurst.last_valid_line+1, :]
-
-            curData = curMap[curBurst.first_valid_line:curBurst.last_valid_line+1, :]
-
-            im1 = topData[-olap:, :]
-            im2 = curData[:olap, :]
-
-            if method == 'avg':
-                data = 0.5*(im1 + im2)
-            elif method == 'top':
-                data = im1
-            elif method == 'bot':
-                data = im2
-            else:
-                raise Exception('Method should be top/bot/avg')
-
-            outArray[linecount:linecount+olap, :] = data
-
-            tlim = olap
+            merged_arr[merge_start_index:merge_start_index+burst_overlap, :] = data
         else:
-            tlim = 0
+            burst_start_index = 0
 
-        linecount += tlim
+        merge_start_index += burst_start_index
 
-        if index != (len(burst_ids)-1):
-            botLimit = azReferenceOff[index+1]
-
-            olap = curLimit[1] - botLimit[0]
-
-            if olap < 0:
-                raise Exception('No Burst Overlap')
-
-            blim = botLimit[0] - curLimit[0]
+        if index != (num_bursts-1):
+            next_burst_limit = az_reference_offsets[index+1]
+            burst_overlap = burst_limit[1] - next_burst_limit[0]
+            if burst_overlap < 0:
+                raise ValueError(f'No overlap between bursts {index} and {index+1} in swath {swath}')
+            burst_end_index = next_burst_limit[0] - burst_limit[0]
         else:
-            blim = curBurst.last_valid_line - curBurst.first_valid_line + 1
+            burst_end_index = burst.last_valid_line - burst.first_valid_line + 1
 
-        lineout = blim - tlim
+        burst_length = burst_end_index - burst_start_index
 
-        curData = curMap[curBurst.first_valid_line:curBurst.last_valid_line+1, :]
-        outArray[linecount:linecount+lineout, :] = curData[tlim:blim, :]
+        burst_arr = burst_arr[burst.first_valid_line:burst.last_valid_line+1, :][burst_start_index:burst_end_index]
+        merged_arr[merge_start_index:merge_start_index+burst_length, :] = burst_arr
 
-        linecount += lineout
+        merge_start_index += burst_length
 
-    driver = gdal.GetDriverByName('ENVI')
-    outRaster = driver.Create("swath_iw"+str(swath)+".slc", width, nLines, 1, gdal.GDT_CFloat32)
-    outRaster.SetGeoTransform(tran)
-    outRaster.SetProjection(proj)
-    outband = outRaster.GetRasterBand(1)
-    outband.WriteArray(outArray)
-    outband.FlushCache()
-    del outRaster, outband
+    output_path = "swath_iw"+str(swath)+".slc"
+
+    write_slc_gdal(
+        merged_arr,
+        output_path,
+        tran,
+        proj,
+        num_rng_samples,
+        num_az_lines
+    )
 
 
 def get_topsinsar_config():
